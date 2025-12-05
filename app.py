2c41ee820eea9ed7e125986fbd9bc0a5d0ee08ad
Ryan Smith <147011183+rps9@users.noreply.github.com>
2025-12-05 15:54:08 -0500

    updating version number

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from typing import Any, Dict, List
 
 from chris_plugin import chris_plugin
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 SUMMARY_TOKEN = "[fedmed-supernode-app] SUMMARY "
 DEFAULT_SUPERLINK_PORT = 9092
